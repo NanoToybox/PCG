922d1cd818ef66dd11bdc27d43a2c441bde7eb03
David <wyatwork@126.com>
2017-11-30 00:25:26 +0800

    This, sadly, is a failed attempt.

--- Fractal.py
+++ Fractal.py
@@ -9,23 +9,24 @@ _canvas = canvas.canvas()
 INNER_RECT_RATIO = 0.4/2
 
 # left_bottom corner is (0,0)
-def drawFractal(p0, p1, p2, p3):
-	length = max(abs(p0[0]-p1[0]),abs(p0[1]-p1[1]))
-	if (length < 20):
+def drawFractal(p0, p1, p2, p3, useX = 1):
+	length = min(abs(p0[0]-p1[0]),abs(p2[0]-p3[0])) if useX == 1 else min(abs(p0[1]-p2[1]),abs(p1[1]-p3[1]))
+	#print("border ",p0,p1,p2,p3)
+	#print(length)
+	if (length < 1):
 		return
-	print("border ",p0,p1,p2,p3)
 
-	inner_rect = ((p0[0]+p1[0])/2-length*INNER_RECT_RATIO, 
-                      (p0[1]+p2[1])/2-length*INNER_RECT_RATIO,
-                      (p0[0]+p1[0])/2+length*INNER_RECT_RATIO,
-                      (p0[1]+p2[1])/2+length*INNER_RECT_RATIO)
+	inner_rect = ((p0[0]+p1[0]+p2[0]+p3[0])/4-length*INNER_RECT_RATIO, 
+                      (p0[1]+p1[1]+p2[1]+p3[1])/4-length*INNER_RECT_RATIO,
+                      (p0[0]+p1[0]+p2[0]+p3[0])/4+length*INNER_RECT_RATIO,
+                      (p0[1]+p1[1]+p2[1]+p3[1])/4+length*INNER_RECT_RATIO)
 	inner_length = inner_rect[2] - inner_rect[0]
-	print("inner ", inner_rect)
+	#print("inner ", inner_rect)
 
 	_canvas.stroke(path.rect(#(x, y, w, h)
 		inner_rect[0],inner_rect[1],abs(inner_rect[2]-inner_rect[0]),abs(inner_rect[3]-inner_rect[1])
 		), [style.linewidth.Thick])
-	
+
 	inner_lines = [
 		(inner_rect[0],inner_rect[1],p0[0],p0[1]),
 		(inner_rect[2],inner_rect[1],p1[0],p1[1]),
@@ -44,12 +45,12 @@ def drawFractal(p0, p1, p2, p3):
 			[style.linewidth.Thick]
 			)
 		next_i = index + 1 if index < 3 else 0
-		drawFractal(
-			(inner_lines[index][0],inner_lines[index][1]),
-			(inner_lines[next_i][0],inner_lines[next_i][1]),
-			(inner_lines[index][2],inner_lines[index][3]),
-			(inner_lines[next_i][2],inner_lines[next_i][3])
-		)
+	#Am i fucking stupid? why wouldn't I just go up down left right?
+	drawFractal((inner_rect[0],inner_rect[3]), (inner_rect[2],inner_rect[3]), p2, p3) #up
+	drawFractal(p0, p1, (inner_rect[0],inner_rect[1]), (inner_rect[2],inner_rect[1])) #down
+	drawFractal(p0, (inner_rect[0],inner_rect[1]), p2, (inner_rect[0],inner_rect[3]), 0) #left
+	drawFractal((inner_rect[2],inner_rect[1]), p1, (inner_rect[2],inner_rect[3]), p3, 0) #right
+
 drawFractal((0,0),(100,0),(0,100),(100,100))
 
 _canvas.writePDFfile()
